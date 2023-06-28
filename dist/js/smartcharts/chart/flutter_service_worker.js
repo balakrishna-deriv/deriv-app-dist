63939cc34c382c79f00b3fb0a86926cca409c769
balakrishna-binary <bala.krishna@regentmarkets.com>
2023-06-28 10:07:02 +0800

    new build

--- dist/js/smartcharts/chart/flutter_service_worker.js
+++ dist/js/smartcharts/chart/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "1756499d77011c0be564a414e567db76",
-"index.html": "7b736e9885bebd9bb8fab90dab3b9bb5",
-"/": "7b736e9885bebd9bb8fab90dab3b9bb5",
-"main.dart.js": "f0d33dbeb1bf679299711a157edf1053",
+"index.html": "473d8543317e82727962cb1e3a148518",
+"/": "473d8543317e82727962cb1e3a148518",
+"main.dart.js": "09819a397214fc1e25909f23ef7df75a",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "a06b13fcde5dfd1e240278072e116d12",
